1cfd6b6bfedcc92a02aae660c9ff7dcb49f194fa
Charlotte Towell <106140733+charlottetowell@users.noreply.github.com>
2025-07-14 18:40:05 +1000

    add initial callbacks + conf

--- main.lua
+++ main.lua
@@ -1,3 +1,21 @@
 function love.draw()
     love.graphics.print('Hello World!', 400, 300)
 end
+
+function love.load()
+   love.graphics.setNewFont(12)
+   love.graphics.setColor(0,0,0)
+   love.graphics.setBackgroundColor(255,255,255)
+end
+
+function love.focus(f) gameIsPaused = not f end
+
+function love.update(dt)
+	if gameIsPaused then return end
+
+	-- The rest of your love.update code goes here
+end
+
+function love.quit()
+  print("Thanks for playing! Come back soon!")
+end
