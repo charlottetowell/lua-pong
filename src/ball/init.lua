1156be0ca99b80b3fe68f9cd1e77d43ebb97b6e4
Charlotte Towell <106140733+charlottetowell@users.noreply.github.com>
2025-07-15 19:09:13 +1000

    randomise ball starting velocity

--- src/ball/init.lua
+++ src/ball/init.lua
@@ -15,8 +15,18 @@ function Ball:new()
     ball.x = WINDOW_WIDTH_PX / 2
     ball.y = WINDOW_HEIGHT_PX / 2
     ball.vel = {}
-    ball.vel.x = -8
-    ball.vel.y = 0
+    ball.vel.x = math.random(8,10)
+    ball.vel.y = math.random(0,3)
+    if math.random() <= 0.5 then
+        ball.vel.x = ball.vel.x * -1
+    else 
+        ball.vel.x = ball.vel.x 
+    end
+    if math.random() <= 0.5 then
+        ball.vel.y = ball.vel.y * -1
+    else
+        ball.vel.y = ball.vel.y
+    end
     ball.height = 30
     ball.width = 30
 
