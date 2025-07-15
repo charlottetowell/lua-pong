f2ae7f23700f45dbb068ce6413a51b69c6d6fef9
Charlotte Towell <106140733+charlottetowell@users.noreply.github.com>
2025-07-15 19:46:45 +1000

    update player update func

--- src/player/update.lua
+++ src/player/update.lua
@@ -2,17 +2,17 @@ function Player:update()
     require "src/player/paddle/move"
     if self.role == 1 then
         if love.keyboard.isDown("w") then
-            players[1].paddle:move(-1)
+            self.paddle:move(-1)
         end
         if love.keyboard.isDown("s") then
-            players[1].paddle:move(1)
+            self.paddle:move(1)
         end
     elseif self.role == 2 then
         if love.keyboard.isDown("up") then
-            players[2].paddle:move(-1)
+            self.paddle:move(-1)
         end
         if love.keyboard.isDown("down") then
-            players[2].paddle:move(1)
+            self.paddle:move(1)
         end
     end
 end
