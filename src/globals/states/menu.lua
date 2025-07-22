178c9050cfc48218b1f1ec92117471c5deff6f3d
Charlotte Towell <106140733+charlottetowell@users.noreply.github.com>
2025-07-22 16:16:22 +1000

    add (ugly) start game button - graphics only

--- src/globals/states/menu.lua
+++ src/globals/states/menu.lua
@@ -18,5 +18,16 @@ local scaleFactor = 600 / width;
 function menuState:draw()
     love.graphics.setColor(1,1,1);
     love.graphics.draw(menuBackground, 0, 0, 0, scaleFactor, scaleFactor);
-    love.graphics.setBackgroundColor(0.35,0.2,0.6)
+    love.graphics.setBackgroundColor(0.35,0.2,0.6);
+
+    --draw 'start game' button
+    local startGameButton = {
+        width=200,
+        height=50,
+        y=450,
+        text="Start Game"
+    }
+    love.graphics.rectangle("fill",(600-startGameButton.width)/2,startGameButton.y,startGameButton.width,startGameButton.height);
+    love.graphics.setColor(0,0,0);
+    love.graphics.print(startGameButton.text,(600-startGameButton.width)/2,startGameButton.y,0,2,2)
 end
